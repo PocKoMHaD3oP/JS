543f7b4ff1ca2ed7c0d4ddffddfd208edd9812bb
PocKoMHaD3oP <78443276+PocKoMHaD3oP@users.noreply.github.com>
2021-02-02 22:28:59 +0300

    Create code.js

---
+++ code.js
@@ -0,0 +1,10 @@
+window.addEventListener('load', function () {
+	let page = document.getElementById("change_page");
+	let style = document.getElementById("change_style");
+	page.addEventListener('click', function (){
+		window.location.href = "https://en.wikipedia.org/wiki/The_Holocaust";
+		});
+	style.addEventListener('click', function (){
+		document.body.style.backgroundColor = 'yellow');
+	});
+});
